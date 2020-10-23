bf96e675efb73df773621d48e00a1d0943f24091
Fatih Egriboz <fatih.egriboz@bilgi.edu.tr>
2020-10-23 14:42:44 +0300

    change title

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import React from "react"
-import Button from "../components/button"
+import React from 'react'
+import Button from '../components/button'
 
 function HomePage() {
-  return <div>
-    <h1>
-      Welcome to Next.js!
-    </h1>
-    <Button>Button</Button>
-  </div>
+  return (
+    <div>
+      <h1>Welcome to Next.js</h1>
+      {/* <Button>Button</Button> */}
+    </div>
+  )
 }
 
 export default HomePage
